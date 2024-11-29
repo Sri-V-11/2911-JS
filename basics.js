36ecc027d0da1c40dcb4e697f65e677b855656bf
Srijan.C.V <srijancv11@gmail.com>
2024-11-30 00:29:28 +0530

    container config done

---
+++ basics.js
@@ -0,0 +1,10 @@
+const account_Id = 1234532
+var accountEmail = "aa@gmal.com"
+let password = "1323"
+accountcity = "Rampur"
+let accountstate
+console.table([account_Id,accountEmail,password,accountcity,accountstate])
+/*
+  const-keyword for constants , var-for variables(NOT PREFERED BECAUSE OF ISSUE IN BLOCK AND FUNCTION SCOPE.)
+  LET- KEYWORD for initialising a variable and then it can be taken anything if not initialisied it shoes undefined.
+  */
