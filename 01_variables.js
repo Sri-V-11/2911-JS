913d8e05ed02b0d44eb286581277316510568730
Srijan.C.V <srijancv11@gmail.com>
2024-11-30 00:35:07 +0530

    CONTAINER CONFIG DONE

--- 01_variables.js
+++ 01_variables.js
@@ -3,7 +3,9 @@ var accountEmail = "aa@gmal.com"
 let password = "1323"
 accountcity = "Rampur"
 let accountstate
-console.table([account_Id,accountEmail,password,accountcity,accountstate])
+/* To print the entire varibls we use this command of TABLE.
+*/
+console.table([account_Id,accountEmail,password,accountcity,accountstate]) 
 /*
   const-keyword for constants , var-for variables(NOT PREFERED BECAUSE OF ISSUE IN BLOCK AND FUNCTION SCOPE.)
   LET- KEYWORD for initialising a variable and then it can be taken anything if not initialisied it shoes undefined.
